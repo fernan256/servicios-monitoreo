7c3ce0b0451cee9915fe6d8e1c8f8044a46249ef
Diego <diego.mayorga@lopsa.com.ar>
2016-02-11 03:00:00 -0300

    feat(DomainController): update function to laravel 5

--- app/Http/Controllers/DomainsController.php
+++ app/Http/Controllers/DomainsController.php
@@ -18,29 +18,20 @@ class DomainsController extends Controller
     $domains = Domain::with('records')->get();
     return $domains;
   }
-  public function showRecord(Request $request) {
-    $id = $request->input('id');
-    $records = Record::where('domain_id', '=', $id)->get();
+  public function showRecord($id) {
+    $records = Record::where('domain_id', $id)->get();
     return $records;
   }
-  public function showDomain(Request $request) {
-    $id = $request->input('id');
-    $records = Domain::where('id', '=', $id)->get();
-    return $records;
+  public function show($id) {
+    $domain = Domain::where('id', $id)->get();
+    return $domain;
   }
-  public function destroy(Request $request) {
-    $id = $request->input('id');
-    $aux = $request->input('hidden');
-    if ($aux == 'record' ) {
-      $records = Record::where('id', '=', $id)-> delete();
-      //return Redirect::to('list_zones')->with('notice', 'El registro ha sido eliminado correctamente.');
-    } else {
-      $domain = Domain::find($id);
-      $domain_id = $id;
-      $zones = Zone::where('domain_id', '=', $domain_id)-> delete();
-      $records = Record::where('domain_id', '=', $domain_id)-> delete();
-      $domain->delete();
-      return response()->json('ok');
-    }
+  public function destroy($id) {
+    $domain = Domain::find($id);
+    $domain_id = $id;
+    $zones = Zone::where('domain_id', '=', $domain_id)-> delete();
+    $records = Record::where('domain_id', '=', $domain_id)-> delete();
+    $domain->delete();
+    return response()->json('success', 200);
   }
 }
