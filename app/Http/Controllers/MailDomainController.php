e20acbb194edf48dfc4946e36fbb9af4e84333ac
Diego <diego.mayorga@lopsa.com.ar>
2016-02-11 03:04:28 -0300

    feat(MailDomainController): add CRUD for mail domain

--- app/Http/Controllers/MailDomainController.php
+++ app/Http/Controllers/MailDomainController.php
@@ -11,9 +11,50 @@ use App\Models\Maildomain;
 
 class MailDomainController extends Controller
 {
-    public function index()
-    {
-      $maildomain = Maildomain::all();
-      return $maildomain;
+    public function index() {
+      $mailDomain = Maildomain::all();
+      return $mailDomain;
     }
+
+    public function show($id) {
+      $mailDomain = Maildomain::where('pkid', $id)->get();
+      return $mailDomain;
+    }
+
+    public function store(Request $request) {
+      $domainName = $request->input('data.domain');
+      if($domainName !== null) {
+        $domain = Maildomain::where('domain', '=', $domainName)->first();
+        if($domain !== null) {
+          return response()->json('El dominio ya existe');
+        } else {
+          $mailDomain = new Maildomain();
+          $mailDomain->domain = $request->input('data.domain');
+          $mailDomain->enabled = $request->input('data.enabled');
+          $mailDomain->save();
+        }
+      }
+    }
+
+  public function destroy($id) {
+    $maildomain = Maildomain::where('pkid', $id)-> delete();
+    return response()->json('ok');
+  }
+
+  public function update(Request $request) {
+    $pkid = $request->input('data.pkid');
+    $mailDomain = new Maildomain();
+    $mailDomain->domain = $request->input('data.domain');
+    if($request->input('data.enabled') == ''){
+      $mailDomain->enabled = '0';
+    } else {
+      $mailDomain->enabled ='1';
+    }
+
+    //Update query
+    Maildomain::where('pkid', $pkid)
+                ->update(['domain' => $mailDomain->domain, 'enabled' => $mailDomain->enabled]);
+
+    return response()->json('Actualizado');
+  }
 }
